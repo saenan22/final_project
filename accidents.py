bff05c52a6e2c77039ea6a682743d54032fe0fd0
saenan22 <yellow902@g.hongik.ac.kr>
2024-12-13 19:08:11 +0900

    Update accidents.py

--- accidents.py
+++ accidents.py
@@ -10,7 +10,7 @@ file_path = r"https://raw.githubusercontent.com/saenan22/final_project/refs/head
 df = pd.read_csv(file_path, header=3)
 
 # 2. GeoJSON 파일 불러오기
-geojson_file = r"BND_SIGUNGU_PG.json"
+geojson_file = r"https://raw.githubusercontent.com/saenan22/final_project/refs/heads/main/BND_SIGUNGU_PG.json"
 with open(geojson_file, 'r', encoding='utf-8') as f:
     geojson = json.load(f)
 
