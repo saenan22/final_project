eda5bf0f4be3e52bd7d88c060ba7ba6feb68bca8
saenan22 <yellow902@g.hongik.ac.kr>
2024-12-13 21:04:25 +0900

    Update accidents.py

--- accidents.py
+++ accidents.py
@@ -155,8 +155,7 @@ st.write(df_filtered)
 
 # 선택된 필터 옵션과 관련된 다른 분석 추가 (예시)
 st.subheader("선택된 지역에 따른 사고 통계")
-st.bar_chart(df_filtered.groupby(["시도", "시군구"])["사고[건]"].sum())
-
+st.bar_chart(df_filtered.set_index("시도")["사고[건]"].sum())
 
 # 사이드바에 지역 선택 추가
 st.sidebar.subheader("지역 선택")
@@ -180,5 +179,5 @@ st.write(df_filtered)
 
 # 필터링된 데이터에 대한 차트 출력
 st.subheader("선택된 지역에 따른 사고 통계")
-st.bar_chart(df_filtered.groupby(["시도", "시군구"])["사고[건]"].sum())
+st.bar_chart(df_filtered.set_index("시도")["사고[건]"].sum())
 
